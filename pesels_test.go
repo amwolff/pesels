fcde95531da93fe19d2956943a5bfd4f931540f0
Artur M. Wolff <artur.m.wolff@gmail.com>
2019-09-17 22:03:57 +0200

    Cover the code with tests

--- pesels_test.go
+++ pesels_test.go
@@ -1,97 +1,29 @@
 package pesels
 
 import (
+	"math"
 	"reflect"
 	"testing"
+	"time"
 )
 
 func Test_btoi(t *testing.T) {
-	type args struct {
-		b byte
-	}
-	tests := []struct {
-		name  string
-		args  args
-		want  int
-		want1 bool
-	}{
-		{
-			"1",
-			args{'1'},
-			1,
-			true,
-		},
-		{
-			"2",
-			args{'2'},
-			2,
-			true,
-		},
-		{
-			"3",
-			args{'3'},
-			3,
-			true,
-		},
-		{
-			"4",
-			args{'4'},
-			4,
-			true,
-		},
-		{
-			"5",
-			args{'5'},
-			5,
-			true,
-		},
-		{
-			"6",
-			args{'6'},
-			6,
-			true,
-		},
-		{
-			"7",
-			args{'7'},
-			7,
-			true,
-		},
-		{
-			"8",
-			args{'8'},
-			8,
-			true,
-		},
-		{
-			"9",
-			args{'9'},
-			9,
-			true,
-		},
-		{
-			"0",
-			args{'0'},
-			0,
-			true,
-		},
-		{
-			"-",
-			args{'-'},
-			0,
-			false,
-		},
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			got, got1 := btoi(tt.args.b)
-			if got != tt.want {
-				t.Errorf("btoi() got = %v, want %v", got, tt.want)
+	var b byte
+	for ; ; b++ {
+		d, ok := btoi(b)
+		switch b {
+		case '1', '2', '3', '4', '5', '6', '7', '8', '9', '0':
+			if !(d >= 0 && d <= 9 && ok) {
+				t.Errorf("btoi() = %v, want %c", d, b)
 			}
-			if got1 != tt.want1 {
-				t.Errorf("btoi() got1 = %v, want %v", got1, tt.want1)
+		default:
+			if !(d == 0 && !ok) {
+				t.Errorf("btoi() = %v, want %v", d, 0)
 			}
-		})
+		}
+		if b == math.MaxUint8 { // A typical loop would overflow b.
+			break
+		}
 	}
 }
 
@@ -111,18 +43,23 @@ func TestValid(t *testing.T) {
 		},
 		{
 			"contains non-digit characters",
-			args{"520810485.2"},
+			args{"520810485#2"},
 			false,
 		},
 		{
 			"contains non-digit characters",
-			args{"5208104853."},
+			args{"5208104853@"},
 			false,
 		},
 		{
 			"wrong checksum",
 			args{"44051401358"},
 			false,
+		},
+		{
+			"OK",
+			args{"70010198266"},
+			true,
 		},
 	}
 	for _, tt := range tests {
@@ -149,6 +86,116 @@ func TestParse(t *testing.T) {
 			args{"44051401358"},
 			PESEL{},
 			true,
+		},
+		{
+			"1800-01-01/0186/F/7",
+			args{"00810101867"},
+			PESEL{
+				time.Date(1800, time.January, 1, 0, 0, 0, 0, loc),
+				186,
+				Female,
+				7,
+			},
+			false,
+		},
+		{
+			"1800-10-01/1307/M/9",
+			args{"00900113079"},
+			PESEL{
+				time.Date(1800, time.October, 1, 0, 0, 0, 0, loc),
+				1307,
+				Male,
+				9,
+			},
+			false,
+		},
+		{
+			"1900-01-01/0970/F/0",
+			args{"00010109700"},
+			PESEL{
+				time.Date(1900, time.January, 1, 0, 0, 0, 0, loc),
+				970,
+				Female,
+				0,
+			},
+			false,
+		},
+		{
+			"1900-10-01/1327/M/3",
+			args{"00100113273"},
+			PESEL{
+				time.Date(1900, time.October, 1, 0, 0, 0, 0, loc),
+				1327,
+				Male,
+				3,
+			},
+			false,
+		},
+		{
+			"2000-01-01/1780/F/6",
+			args{"00210117806"},
+			PESEL{
+				time.Date(2000, time.January, 1, 0, 0, 0, 0, loc),
+				1780,
+				Female,
+				6,
+			},
+			false,
+		},
+		{
+			"2000-10-01/1511/M/0",
+			args{"00300115110"},
+			PESEL{
+				time.Date(2000, time.October, 1, 0, 0, 0, 0, loc),
+				1511,
+				Male,
+				0,
+			},
+			false,
+		},
+		{
+			"2100-01-01/1072/F/0",
+			args{"00410110720"},
+			PESEL{
+				time.Date(2100, time.January, 1, 0, 0, 0, 0, loc),
+				1072,
+				Female,
+				0,
+			},
+			false,
+		},
+		{
+			"2100-10-01/0295/M/0",
+			args{"00500102950"},
+			PESEL{
+				time.Date(2100, time.October, 1, 0, 0, 0, 0, loc),
+				295,
+				Male,
+				0,
+			},
+			false,
+		},
+		{
+			"2200-01-01/0220/F/6",
+			args{"00610102206"},
+			PESEL{
+				time.Date(2200, time.January, 1, 0, 0, 0, 0, loc),
+				220,
+				Female,
+				6,
+			},
+			false,
+		},
+		{
+			"2200-10-01/0909/M/0",
+			args{"00700109090"},
+			PESEL{
+				time.Date(2200, time.October, 1, 0, 0, 0, 0, loc),
+				909,
+				Male,
+				0,
+			},
+			false,
 		},
 	}
 	for _, tt := range tests {
